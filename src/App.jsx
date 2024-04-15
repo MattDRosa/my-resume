fc2918e51dc62d7f024395fa07bad095a21c4bb8
Matheus Rosa <matheus.rosa@2cloud.com.br>
2024-04-15 09:14:33 -0300

    added gitlab image and removed unused lines

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AppRouter from "./components/router/Router";
 
 function App() {
   return (
-    // <div className="bg-gradient-to-tl from-purple-950 via-indigo-950 to-black via-30% z-0 min-h-screen h-full pt-10 mb-auto">
     <div className="bg-black z-0 min-h-screen h-full pt-10 mb-auto">
       <AppRouter>
       </AppRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import Footer from "../components/footer/Footer";
 export default function Home() {
 
     const certifications = 7;
-    const technologies = 4;
+    const technologies = 5;
 
 	return (
         <>
             <div className="mx-auto mt-10 rounded-xl max-w-7xl bg-neutral-800/30">
                 <MainContent 
                     title={"Matheus Rosa"} 
-                    paragraph={"Hi! My name is Matheus Araujo da Rosa, I'm a Software Engineer with a high expertise in DevOps practices and AWS Cloud. Throughout my journey, I worked with the best sides of development and operations, working with many different cloud environments and always searching for inovation and modernization and following the best practices."}>
+                    paragraph={"Hi! My name is Matheus Araujo da Rosa, I'm a DevOps Engineer with a high expertise in DevOps practices and AWS Cloud. Throughout my journey, I worked with the best sides of development and operations, working with many different cloud environments and always searching for inovation and modernization following the best practices."}>
                 </MainContent>
                 <ImageDisplay title={"My certifications"} assetsFolder={"certifications"} amount={certifications}></ImageDisplay>
                 <ImageDisplay title={"Technologies"} assetsFolder={"technologies"} amount={technologies}></ImageDisplay>
